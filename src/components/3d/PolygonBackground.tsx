f38f5036add76e4c614086a149be519f7688a5c9
Julian Goldbach <jfg.msg@protonmail.com>
2023-08-14 15:13:46 +0200

    Fixed TS error on PolygonBackground

--- src/components/3d/PolygonBackground.tsx
+++ src/components/3d/PolygonBackground.tsx
@@ -4,48 +4,48 @@ import { Suspense, useRef } from "react"
 import THREE from "three"
 
 
-const PolygonMaterial = shaderMaterial(
-    { uTime: 6.0 },
-    `
-    //vertex shader
-    varying vec3 pos;
-    varying float calcZ;
-    uniform float uTime;
-    void main() {
-        pos = position;
-        calcZ = (sin((position.x - 18.0) * (uTime / 12.0)) * sin((position.y - 8.0) * (uTime / 12.0)))/ 8.0;
-        gl_Position = projectionMatrix * modelViewMatrix * 
-            vec4(
-                position.x, 
-                position.y, 
-                calcZ, 
-                1
-            );
-    }
-    `,
-    `
-    //fragment shader
-    varying vec3 pos;
-    varying float calcZ;
-    float depth;
-    void main() {
-        depth = calcZ * 10.0;
-        gl_FragColor = vec4(depth, depth, depth, 1.0);
-    }
-    `
-)
+const vertexShader = `
+varying vec3 pos;
+varying float calcZ;
+uniform float uTime;
+void main() {
+    pos = position;
+    calcZ = (sin((position.x - 18.0) * (uTime / 12.0)) * sin((position.y - 8.0) * (uTime / 12.0)))/ 8.0;
+    gl_Position = projectionMatrix * modelViewMatrix * 
+        vec4(
+            position.x, 
+            position.y, 
+            calcZ, 
+            1
+        );
+}
+`
+
+const fragmentShader = `
+varying vec3 pos;
+varying float calcZ;
+float depth;
+void main() {
+    depth = calcZ * 10.0;
+    gl_FragColor = vec4(depth, depth, depth, 1.0);
+}
+`
+
+const shaderProps = {uTime: 6.0,}
+
+const PolygonMaterial = shaderMaterial(shaderProps, vertexShader, fragmentShader)
 
 extend({ PolygonMaterial })
 
-type PolygonMat = {
+/* type PolygonMat = {
     uTime: number
-} & JSX.IntrinsicElements['shaderMaterial']
+} & JSX.IntrinsicElements['shaderMaterial'] */
 
 
 declare global {
     namespace JSX {
         interface IntrinsicElements {
-            polygonMaterial: PolygonMat
+            polygonMaterial: ReactThreeFiber.Node<typeof PolygonMaterial & JSX.IntrinsicElements['shaderMaterial'], typeof PolygonMaterial>
         }
     }
 }
@@ -53,17 +53,18 @@ declare global {
 
 
 function PolygonMesh() {
-    const matRef = useRef<PolygonMat>(null)
+    const matRef = useRef<typeof PolygonMaterial>(null)
     useFrame((state, delta) => {
-        if (matRef.current) {
-            matRef.current.uTime += delta / 1.5
+        const mat = matRef.current
+        if (mat) {
+            mat.uTime += delta / 1.5
         }
     })
 
     return (
         <mesh>
             <planeGeometry args={[18.0, 8.0, 18.0, 8.0]} />
-            {/* <polygonMaterial ref={matRef} wireframe /> */}
+            <polygonMaterial ref={matRef} wireframe />
         </mesh>
     )
 }
